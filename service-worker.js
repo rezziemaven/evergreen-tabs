3af9338b140396ab3e258afd5645bbd6ad640a06
Sherezz Grant <rezziemaven@gmail.com>
2024-10-24 14:36:55 -0400

    Comment out logs

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // add pinned tabs when window is opened
 chrome.windows.onCreated.addListener(async (window) => {
   const tabs = await chrome.tabs.query({ windowId: window.id });
-  console.log('my tabs: ', tabs);
+  // console.log('my tabs: ', tabs);
   const storage = await chrome.storage.sync.get(['evergreenTabLinks']);
   if (storage.evergreenTabLinks.length && !window.incognito) {
     storage.evergreenTabLinks.forEach(
@@ -9,10 +9,10 @@ chrome.windows.onCreated.addListener(async (window) => {
         const tabExists = tabs.some((tab) => {
           // strip out http or https:// from link
           const urlMinusScheme = link.url.split('//')[1];
-          console.log({ tabURL: tab.url, linkURL: urlMinusScheme });
+          // console.log({ tabURL: tab.url, linkURL: urlMinusScheme });
           return tab.url.includes(urlMinusScheme);
         });
-        console.log('exists? ', tabExists);
+        // console.log('exists? ', tabExists);
         if (!tabExists)
           chrome.tabs.create({
             url: link.url,
