a663ba7a1b0ff4cd247dd7b74d282cbd825a216d
Sherezz Grant <rezziemaven@gmail.com>
2024-10-24 16:06:44 -0400

    Replace li with div element

--- src/components/LinkListItem.jsx
+++ src/components/LinkListItem.jsx
@@ -1,11 +1,11 @@
 export function LinkListItem(props) {
   return (
-    <li className="" key={props.id}>
+    <div className="link-item">
       <button className="move-item">&#8645;</button>
       <span className="link">{props.url}</span>
       <button onClick={() => props.onRemove(props.id)} className="delete-item">
         &times;
       </button>
-    </li>
+    </div>
   );
 }
